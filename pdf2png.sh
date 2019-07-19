6f2d874f604c69b4f2240d65c30e6de684ee1fb5
alonzi <pete.alonzi@gmail.com>
2019-07-18 22:30:33 -0400

    updated for dvcs

---
+++ pdf2png.sh
@@ -0,0 +1,9 @@
+convert           \
+   -verbose       \
+   -density 150   \
+   -trim          \
+    diagram-crop.pdf      \
+   -quality 100   \
+   -flatten       \
+   -sharpen 0x1.0 \
+    diagram-crop.jpg
